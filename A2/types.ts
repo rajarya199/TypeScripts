1486976290c546f6159761c7da76296ac2d822e0
rajarya199 <aryalrajan078@gmail.com>
2025-12-04 20:43:39 +0545

    type aalias-types

--- A2/types.ts
+++ A2/types.ts
@@ -12,3 +12,25 @@ let a:ClassmatesInStudents={
     name:"hari",
     section:"sun"
 }
+
+//type can defined any type,not just object
+//primitives,union,intersection,function,tuples,object types
+
+
+//object type
+type User={
+    name:string,
+    age:number
+}
+
+//union type
+type Score=string | number;
+let marks:Score=101;
+marks="A-";
+
+//function type
+type Add=(a:number,b:number)=>number;
+
+const sum:Add=(x,y)=>x+y;
+
+
