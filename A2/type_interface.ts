76294e7b1e17d33090efbb897b82f1eb8e456d06
rajarya199 <aryalrajan078@gmail.com>
2025-12-05 23:31:24 +0545

    interface extends

--- A2/type_interface.ts
+++ A2/type_interface.ts
@@ -11,3 +11,24 @@ interface Human{
 }  // so human interfsce have both name and age properties
 
 const h:Human={name:"Ram",age:25}
+
+
+interface Food{
+    name:string;
+    price:number;
+}
+
+interface MithaFood extends Food{
+    verySweet:boolean
+} //name,price,verySweet
+
+interface KhattaFood extends Food{
+    verySour:boolean
+} //name,price,verySour
+
+function getMithaai(mithaai:MithaFood){
+    mithaai.verySweet
+}
+function getKhattaFood(khattaFood:KhattaFood){
+    khattaFood.verySour
+}
